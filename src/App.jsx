5e32d6d05864df53b0f80a077fcbf4b8ff7d9dea
Joshua Mora <joshuaalexandermora@gmail.com>
2020-09-11 19:48:33 -0400

    finalized

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import logo from './assets/cohete.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <p className="top">Houston ... we had a problem!</p>
+      <main className="main-cont">
+        <img src={logo} className="logo" alt="logo" width="120" />
+      </main>
+      <div className="bottom">
+        <p>404</p>
+        <p>Not Found</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
